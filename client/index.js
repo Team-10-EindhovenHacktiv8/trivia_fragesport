5ed10d2993114d792731ea759056b0bc8da117a9
ramaardanaa <ramaardanaa@gmail.com>
2020-10-29 16:17:21 +0700

    form quiz and fetch Category and Question from API

--- client/index.js
+++ client/index.js
@@ -4,22 +4,47 @@ $(document).ready(() => {
   fetchCategory()
 })
 
+function initquestion(e){
+  e.preventDefault()
+  const idCategory = $('#idcategory').val()
+  const amountQuestion = $('#amountquestion').val()
+  const difficulty = $('#difficulty').val()
+  console.log(idCategory)
+  console.log(amountQuestion)
+  console.log(difficulty)
+
+  $.ajax({
+    method: "POST", 
+    url: SERVER + "/trivia",
+    data: {
+      idCategory,
+      amountQuestion,
+      difficulty
+    }
+  })
+  .done(data => {
+    $("#triviainit").hide()
+    $("#quiz").show()
+    console.log(data)
+  })
+  .fail(err => {
+    console.log(err)
+  })
+
+}
 function fetchCategory(){
   $.ajax({
     method:"GET",
     url: SERVER + "/categories"
   })
   .then(data => {
-    console.log(data)
+    data.forEach(el => {
+      $("#idcategory").append(`
+      <option value="${el.id}">${el.name}</option>
+      `)
+    })
   })
   .fail(err => {
     console.log(err)
-  })
-}
-
-function initquestion(){
-  $.ajax({
-    method:"POST",
-    url: SERVER + "/trivia"
   })
 }
